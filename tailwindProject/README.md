a12b15d86c24966afcd6cdf22ae23df60615e41c
ankitapatil31 <ankitapatil040@gmail.com>
2024-01-25 19:02:44 -0500

    password genrator and copy function

--- tailwindProject/README.md
+++ tailwindProject/README.md
@@ -2,3 +2,35 @@
 # In this Project I have Learn:
 # how to intigate tailwind css package and Used
 # How to Used props component bases 
+# Add background change base on button click color
+# Steps for Install Tailwind CSS with Vite
+    1: Create your project Start by creating a new Vite project if you don’t have one set up already. The most common approach is to    use Create Vite.
+        npm create vite@latest my-project -- --template react
+        cd my-project
+    
+    2: Install Tailwind CSS Install tailwindcss and its peer dependencies, then generate your tailwind.config.js and postcss.config.js files.
+        npm install -D tailwindcss postcss autoprefixer ( Add package )
+        npx tailwindcss init -p ( Create tailwindcssconfig file)
+    
+    3:Configure your template paths Add the paths to all of your template files in your tailwind.config.js file.
+        content: [
+            "./index.html",
+            "./src/**/*.{js,ts,jsx,tsx}",
+        ],
+    
+    4: Add the Tailwind directives to your CSS Add the @tailwind directives for each of Tailwind’s layers to your ./src/index.css file(top)
+        @tailwind base;
+        @tailwind components;
+        @tailwind utilities;
+
+    EX: export default function App() {
+        return (
+            <h1 className="text-3xl font-bold underline">
+            Hello world!
+            </h1>
+        )
+        }
+
+    5: npm run dev
+
+# Refernce Links Used : https://www.devui.io/components , https://tailwindcss.com/docs/guides/vite
--- tailwindProject/src/App.jsx
+++ tailwindProject/src/App.jsx
@@ -1,43 +1,63 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 import Card from "./Component/Card";
 function App() {
-  const [count, setCount] = useState(0);
+  const [color, setColor] = useState("olive")
 
   return (
-    <>
-      <h1 className="bg-green-400 text-black p-4 rounded-xl mb-4">Tailwind CSS</h1>
-      <Card UserName = "Ankita" BtnText="Used me"/>
-      <Card UserName = "Deba"/>
-      {/*
-      <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-        <img
-          class="w-24 h-24 md:roun rounded-full mx-auto"
-          src="https://images.pexels.com/photos/4668513/pexels-photo-4668513.jpeg"
-          alt=""
-          width="384"
-          height="512"
-        />
-        <div class="pt-6 md:p-8 text-center space-y-4">
-          <blockquote>
-            <p class="text-lg font-medium">
-              “Tailwind CSS is the only framework that I've seen scale on large
-              teams. It’s easy to customize, adapts to any design, and the build
-              size is tiny.”
-            </p>
-          </blockquote>
-          <figcaption class="font-medium">
-            <div class="text-sky-500 dark:text-sky-400">Sarah Dayan</div>
-            <div class="text-slate-700 dark:text-slate-500">
-              Staff Engineer, Algolia
-            </div>
-          </figcaption>
+    <div className="w-full w-screen h-screen duration-200"
+    style={{backgroundColor: color}}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button
+          onClick={() => setColor("red")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "red"}}
+          >Red</button>
+          <button
+          onClick={() => setColor("green")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "green"}}
+          >Green</button>
+          <button
+          onClick={() => setColor("blue")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "blue"}}
+          >Blue</button>
         </div>
-      </figure>
-    */}
-    </>
+      </div>
+    </div>
+    // <>
+    //   {/* <h1 className="bg-green-400 text-black p-4 rounded-xl mb-4">Tailwind CSS</h1>
+    //   <Card UserName = "Ankita" BtnText="Used me"/>
+    //   <Card UserName = "Deba"/> */}
+    //   {/*
+    //   <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+    //     <img
+    //       class="w-24 h-24 md:roun rounded-full mx-auto"
+    //       src="https://images.pexels.com/photos/4668513/pexels-photo-4668513.jpeg"
+    //       alt=""
+    //       width="384"
+    //       height="512"
+    //     />
+    //     <div class="pt-6 md:p-8 text-center space-y-4">
+    //       <blockquote>
+    //         <p class="text-lg font-medium">
+    //           “Tailwind CSS is the only framework that I've seen scale on large
+    //           teams. It’s easy to customize, adapts to any design, and the build
+    //           size is tiny.”
+    //         </p>
+    //       </blockquote>
+    //       <figcaption class="font-medium">
+    //         <div class="text-sky-500 dark:text-sky-400">Sarah Dayan</div>
+    //         <div class="text-slate-700 dark:text-slate-500">
+    //           Staff Engineer, Algolia
+    //         </div>
+    //       </figcaption>
+    //     </div>
+    //   </figure>
+    // */}
+    // </>
   );
 }
 
